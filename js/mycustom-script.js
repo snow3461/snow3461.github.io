9d75bb1b304182ad9b19714d52c996fe35b72c78
Administrateur <Administrateur@E6S611D>
2019-09-20 11:06:58 +0200

    Some refactoring on scrolling, and scrollspy

--- js/mycustom-script.js
+++ js/mycustom-script.js
@@ -11,26 +11,47 @@ $(document).ready(
     });
     /* Loader Code End */
 
-    $(window).on("scroll", function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 50) {
-        $(".nav-scroll").addClass("nav-strict");
-      } else {
-        $(".nav-scroll").removeClass("nav-strict");
+    // $(window).on("scroll", function() {
+    //   var scroll = $(window).scrollTop();
+    //   if (scroll >= 50) {
+    //     $(".nav-scroll").addClass("nav-strict");
+    //   } else {
+    //     $(".nav-scroll").removeClass("nav-strict");
+    //   }
+    // });
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000,
+            "easeInOutExpo"
+          );
+          return false;
+        }
       }
     });
 
-    /*
-    |=================
-    | Onepage Nav
-    |================
-    */
-    // outline navar element accordingto current section, applies to class navbar
-    $("#main-navbar").onePageNav({
-      currentClass: "active",
-      changeHash: false,
-      scrollSpeed: 750,
-      scrollThreshold: 0.5
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function() {
+      $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+      target: "#topNav"
     });
 
     /*
